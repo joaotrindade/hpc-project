9722c4394669dfd0007102ada07dc57d2e0631fe
João Trindade <joao.ferreira.trindade@gmail.com>
2016-01-13 20:17:31 +0100

    complete batch ready

--- default/ext/bin/openmp_batch.sh
+++ default/ext/bin/openmp_batch.sh
@@ -37,11 +37,11 @@ mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/80k/model-4
 mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/80k/model-5-5.props
 mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/80k/model-4-8.props
 
-#mpirun -n 4 ./zombie_model zombie_config.props configs/160k/model-2-2.props
-#mpirun -n 9 ./zombie_model zombie_config.props configs/160k/model-3-3.props
-#mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-4.props
-#mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-5-5.props
-#mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-8.props
+mpirun -n 4 ./zombie_model zombie_config.props configs/160k/model-2-2.props
+mpirun -n 9 ./zombie_model zombie_config.props configs/160k/model-3-3.props
+mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-4.props
+mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-5-5.props
+mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-8.props
 # will start 16 MPI tasks with 7 threads each. Note that
 # each node has 28 cores, so 4 tasks must be started 
 # per host.
