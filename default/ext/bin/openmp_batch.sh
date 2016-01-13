5897f0e2c5e49cbb13932b507c316d10a6ea9dab
João Trindade <joao.ferreira.trindade@gmail.com>
2016-01-13 20:06:40 +0100

    batch file 20k, 40k, 80k

--- default/ext/bin/openmp_batch.sh
+++ default/ext/bin/openmp_batch.sh
@@ -13,16 +13,35 @@
 #SBATCH --mail-type=end 
 #SBATCH --mail-user=joao.ferreira.trindade@gmail.com
 #SBATCH --export=NONE 
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 source /etc/profile.d/modules.sh
 
 cd /home/hpc/t1221/t1221ag/hpc-project/default/ext/bin
 export OMP_NUM_THREADS=2
-mpirun -n 4 ./zombie_model zombie_config.props configs/160k/model-2-2.props
-mpirun -n 9 ./zombie_model zombie_config.props configs/160k/model-3-3.props
-mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-4.props
-mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-5-5.props
-mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-8.props
+
+mpirun -n 4 ./zombie_model zombie_config.props configs/20k/model-2-2.props
+mpirun -n 9 ./zombie_model zombie_config.props configs/20k/model-3-3.props
+mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/20k/model-4-4.props
+mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/20k/model-5-5.props
+mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/20k/model-4-8.props
+
+mpirun -n 4 ./zombie_model zombie_config.props configs/40k/model-2-2.props
+mpirun -n 9 ./zombie_model zombie_config.props configs/40k/model-3-3.props
+mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/40k/model-4-4.props
+mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/40k/model-5-5.props
+mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/40k/model-4-8.props
+
+mpirun -n 4 ./zombie_model zombie_config.props configs/80k/model-2-2.props
+mpirun -n 9 ./zombie_model zombie_config.props configs/80k/model-3-3.props
+mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/80k/model-4-4.props
+mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/80k/model-5-5.props
+mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/80k/model-4-8.props
+
+#mpirun -n 4 ./zombie_model zombie_config.props configs/160k/model-2-2.props
+#mpirun -n 9 ./zombie_model zombie_config.props configs/160k/model-3-3.props
+#mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-4.props
+#mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-5-5.props
+#mpiexec -n 32 --perhost 14 ./zombie_model zombie_config.props configs/160k/model-4-8.props
 # will start 16 MPI tasks with 7 threads each. Note that
 # each node has 28 cores, so 4 tasks must be started 
 # per host.
