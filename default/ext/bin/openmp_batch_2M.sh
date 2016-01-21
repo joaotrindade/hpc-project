ea1369d31a211738281c0a5ccdc4065e27d4aa56
João Trindade <joao.ferreira.trindade@gmail.com>
2016-01-21 16:03:36 +0100

    changed batch files

--- default/ext/bin/openmp_batch_2M.sh
+++ default/ext/bin/openmp_batch_2M.sh
@@ -19,11 +19,11 @@ source /etc/profile.d/modules.sh
 cd /home/hpc/t1221/t1221ag/hpc-project/default/ext/bin
 export OMP_NUM_THREADS=2
 
-mpirun -n 4 --perhost 14 ./zombie_model zombie_config.props configs/1M/model-2-2.props
-mpirun -n 9 --perhost 14 ./zombie_model zombie_config.props configs/1M/model-3-3.props
-mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/1M/model-4-4.props
-mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/1M/model-5-5.props
-mpirun -n 32 --perhost 14 ./zombie_model zombie_config.props configs/1M/model-4-8.props
+mpirun -n 4 --perhost 14 ./zombie_model zombie_config.props configs/2M/model-2-2.props
+mpirun -n 9 --perhost 14 ./zombie_model zombie_config.props configs/2M/model-3-3.props
+mpirun -n 16 --perhost 14 ./zombie_model zombie_config.props configs/2M/model-4-4.props
+mpirun -n 25 --perhost 14 ./zombie_model zombie_config.props configs/2M/model-5-5.props
+mpirun -n 32 --perhost 14 ./zombie_model zombie_config.props configs/2M/model-4-8.props
 
 
 # will start 16 MPI tasks with 7 threads each. Note that
