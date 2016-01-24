40a07566baac055c9edd93bd2c9421bd4d3017ff
LRZ-SIM Funktionskennung <t1221ag@mpp2-login5.cos.lrz.de>
2016-01-25 00:08:49 +0100

    Error fix

--- default/src/zombies/ZombieObserver.cpp
+++ default/src/zombies/ZombieObserver.cpp
@@ -70,6 +70,8 @@ void ZombieObserver::go() {
   }
   
   synchronize<AgentPackage>(*this, *this, *this, RepastProcess::USE_LAST_OR_USE_CURRENT);
+  AgentSet<Zombie> zombies;
+AgentSet<Human> humans; 
   
   if (work_per_rank > OMP_TASK_THRESHOLD) {
   	#pragma omp parallel num_threads(2)
@@ -78,22 +80,25 @@ void ZombieObserver::go() {
 		{
   			#pragma omp task
   			{
-  				AgentSet<Zombie> zombies;
+  			//	AgentSet<Zombie> zombies;
   				get(zombies);
-  				zombies.ask(&Zombie::step);
-  				AgentId id(0,0,2);
-  				Zombie* z = who<Zombie>(id);
+  			//	zombies.ask(&Zombie::step);
+  			//	AgentId id(0,0,2);
+  			//	Zombie* z = who<Zombie>(id);
   			}
   			
   			#pragma omp task
   			{
-  				AgentSet<Human> humans;	
+  			//	AgentSet<Human> humans;	
   				get(humans);
-  				humans.ask(&Human::step); 
+  			//	humans.ask(&Human::step); 
   			}
 
   			#pragma omp taskwait
-	
+			zombies.ask(&Zombie::step);
+                        AgentId id(0,0,2);
+                        Zombie* z = who<Zombie>(id);
+			humans.ask(&Human::step); 
   		}
   	}
   } else {
@@ -105,7 +110,7 @@ void ZombieObserver::go() {
   	AgentId id(0,0,2);
   	Zombie* z = who<Zombie>(id);
   	humans.ask(&Human::step);
-  }
+ }
 
   if (_rank == 0) {
     Log4CL::instance()->get_logger("root").log(INFO, "TICK ENDS: " + boost::lexical_cast<string>(RepastProcess::instance()->getScheduleRunner().currentTick()));
